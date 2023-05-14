07174a9448741060abc5702a56c1b44c7c349248
Shaharyar07 <shaharyar.tariq248@gmail.com>
2023-05-14 23:11:32 +0500

    completed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+"use client";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((prompt) => (
+        <PromptCard
+          key={prompt._id}
+          prompt={prompt}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [searchText, setSearchText] = useState("");
+  const [prompts, setPrompts] = useState([]);
 
-export default Feed
+  useEffect(() => {
+    const fetchPrompts = async () => {
+      const res = await fetch("/api/prompt");
+      const data = await res.json();
+
+      setPrompts(data);
+    };
+    fetchPrompts();
+  }, []);
+
+  const handleSearchChange = (e) => {};
+  return (
+    <section className='feed'>
+      <form action='' className='flex w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or username'
+          className='search_input peer'
+          value={searchText}
+          onChange={handleSearchChange}
+        />
+      </form>
+      <PromptCardList data={prompts} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
