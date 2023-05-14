351e01ff0b20850bd20053dc651dca7b1d9dc7b7
Shaharyar07 <shaharyar.tariq248@gmail.com>
2023-05-14 16:45:58 +0500

    create prompt functionality added

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,13 +1,39 @@
 "use client";
+
 import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Form from "@components/Form";
 const page = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [post, setPost] = useState({ prompt: "", tag: "" });
 
-  const handleSubmit = async (e) => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    console.log(post, session?.user.id);
+    try {
+      const res = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+          userId: session?.user.id,
+        }),
+      });
+
+      if (res.ok) {
+        router.push("/");
+      }
+    } catch (e) {
+      throw Error(e.message);
+    } finally {
+      setIsSubmitting(false);
+      setPost({ prompt: "", tag: "" });
+    }
+  };
   return (
     <Form
       type='Create'
--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "@styles/globals.css";
 import { Inter } from "next/font/google";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
+import { Suspense } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,16 +14,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Provider>
-          {" "}
-          <div className='main'>
-            <div className=' gradient' />
-          </div>
-          <main className='app'>
-            <Nav />
-            {children}
-          </main>
-        </Provider>
+        <Suspense fallback={"Loading..."}>
+          <Provider>
+            {" "}
+            <div className='main'>
+              <div className=' gradient' />
+            </div>
+            <main className='app'>
+              <Nav />
+              {children}
+            </main>
+          </Provider>
+        </Suspense>
       </body>
     </html>
   );
